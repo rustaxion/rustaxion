298e6a486859f192d9b5eb3d7d29453a71c87d35
Angelos Bouklis <53124886+ArjixWasTaken@users.noreply.github.com>
2024-11-07 22:28:23 +0200

    db(seed): beatmaps

--- src/database/json/beatmap_difficulty.rs
+++ src/database/json/beatmap_difficulty.rs
@@ -6,9 +6,51 @@ pub struct Difficulty {
     pub notes: u32,
 }
 
+impl Default for Difficulty {
+    fn default() -> Self {
+        Self { diff: 1, notes: 1 }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct BeatmapDifficulty {
     pub easy: Difficulty,
     pub normal: Difficulty,
     pub hard: Difficulty,
+}
+
+impl BeatmapDifficulty {
+    pub fn new(
+        easy_diff: u16,
+        easy_notes: u32,
+        normal_diff: u16,
+        normal_notes: u32,
+        hard_diff: u16,
+        hard_notes: u32,
+    ) -> Self {
+        Self {
+            easy: Difficulty {
+                diff: easy_diff,
+                notes: easy_notes,
+            },
+            normal: Difficulty {
+                diff: normal_diff,
+                notes: normal_notes,
+            },
+            hard: Difficulty {
+                diff: hard_diff,
+                notes: hard_notes,
+            },
+        }
+    }
+}
+
+impl Default for BeatmapDifficulty {
+    fn default() -> Self {
+        Self {
+            easy: Difficulty::default(),
+            normal: Difficulty::default(),
+            hard: Difficulty::default(),
+        }
+    }
 }
--- src/database/json/bpm_range.rs
+++ src/database/json/bpm_range.rs
@@ -5,3 +5,9 @@ pub struct BPMRange {
     pub min: u32,
     pub max: u32,
 }
+
+impl Default for BPMRange {
+    fn default() -> Self {
+        Self { min: 0, max: 0 }
+    }
+}
